76820081198ba7f27dad6fc58da767edf27230d9
Kseniya Kalita <48218587+sunnygirl6@users.noreply.github.com>
2020-12-10 14:47:10 +0300

    Add project files.

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using ChatBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatBot.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        private static int i = 0;
+        private static ChatContext myDB = new ChatContext();
+        
+        public ActionResult Index()
+        {
+            ViewBag.i = i;
+            ViewBag.NextSelected = myDB.Chats.ToList().ElementAt(0);
+
+            return View(myDB.Chats);
+        }
+
+        public ActionResult HandleButtonClick(string mine)
+        {
+            i = (i + 1) % myDB.Chats.Count();
+            ViewBag.i = i;
+            ViewBag.NextSelected = myDB.Chats.ToList().ElementAt(i);
+            return View("Index", myDB.Chats);
+        }
+    }
+}
---
+++ Models/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBot.Models
+{
+    public class Chat
+    {
+        public string UserRequest { get; set; }
+        public string BotResponse { get; set; }
+        public string NextIDs { get; set; }
+        public int Id { get; set; }
+    }
+}
