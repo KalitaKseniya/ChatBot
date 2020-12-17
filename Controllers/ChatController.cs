78379037b377c5f1f571f968319cd66f90698e47
Kseniya Kalita <48218587+sunnygirl6@users.noreply.github.com>
2020-12-17 10:14:54 +0300

    buttons work great

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -31,13 +31,40 @@ namespace ChatBot.Controllers
 
         public ActionResult HandleButtonClick(string mine)
         {
-            int iMine = Int32.Parse(mine);
+            int iMine = Int32.Parse(mine);//id database begins from 1 and in listts- from 0
+            List<Chat> list = new List<Chat>();
+            if (iMine == 0)
+            {
+                list.Add(myDB.Chats.ToList().First());
+                return PartialView("MyForm", list);
+            }
+            ViewBag.NextSelected = myDB.Chats.ToList().Find(a => a.Id == iMine);
+                                                                        //            ViewBag.i = iMine;
+            if (ViewBag.NextSelected.NextIDs == null)
+            {
+                return PartialView("MyForm", list);
+            }
+            var tmp = ViewBag.NextSelected.NextIDs.Split(',') ;
+            List<int> NextIds  = new List<int>();
+            foreach(var item in tmp)
+            {
+                NextIds.Add(Int32.Parse(item));
+            }
+            foreach(var item in NextIds)
+            {
+                list.Add(myDB.Chats.ToList().Find(a => a.Id == item)) ; 
+            }
+            //foreach (var a in myDB.Chats.ToList())
+            //{
 
-            //          i = (i + 1) % myDB.Chats.Count();
-            //iMine = (iMine) % myDB.Chats.Count();
-            ViewBag.i = iMine;
-            ViewBag.NextSelected = myDB.Chats.ToList().ElementAt(iMine);
-            return PartialView("MyForm");
+            //    if (NextIds.Contains(a.Id))
+            //    {
+            //        list.Add(a);
+            //    }
+            //}
+            ////list = (List<Chat>)myDB.Chats.ToList().Where(a => 
+            //    (ViewBag.NextSelected.NextIDs.Contains(a.Id.ToString() + ",") || ViewBag.NextSelected.NextIDs.Contains(a.Id.ToString() + ".")));
+            return PartialView("MyForm", list);
         }
     }
 }
